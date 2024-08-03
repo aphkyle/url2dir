cdd4abff5794616b63aea5ea163fc00f9cb6ce83
aphkyle <aphkyle@gmail.com>
2024-08-03 11:47:13 +0800

    fix: invalid filename, logic

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 const manager = async (downloadItem, suggest)=>{
-    let rules = JSON.parse(localStorage.rules)
-
-    let url = new URL(downloadItem.url)
-    for (const [rule, dir] of Object.entries(rules)) {
-        if (url.hostname == rule){
-            suggest({filename: `${dir}/${downloadItem.filename}`})
-        }
-    }
-    console.log(downloadItem.url)
-    console.log(downloadItem.referrer)
-    suggest()
+    chrome.storage.sync.get("rules", result=>{
+        let rules = result.rules
+        let url = new URL(downloadItem.url)
+        console.log(downloadItem.url)
+        console.log(downloadItem.referrer)
+        console.log(rules)
+        rules.forEach(rule => {
+            // console.log(url.hostname)
+            // console.log(rule.rule)
+            // console.log(url.hostname === rule.rule)
+            // console.log(rule)
+            // console.log(`${rule.dir}/${downloadItem.filename}`)
+            if (url.hostname === rule.rule){
+                suggest({filename: `${rule.dir}/${downloadItem.filename}`, conflictAction: "uniquify"})
+            }
+        });
+    })
 }
 
 chrome.downloads.onDeterminingFilename.addListener((downloadItem, suggest)=>{
     manager(downloadItem, suggest)
+    return true
 })
